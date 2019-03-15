0a8f073a0d3f91225ca5801e1d4fd4dab03d7c53
Steven Karas <steven.karas@gmail.com>
2019-03-15 16:29:22 +0200

    script: pipeline action for the script

--- script.graphs.py
+++ script.graphs.py
@@ -1,43 +1,65 @@
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('collated_results.csv')
-data['Has loose-out-bailiwick glue'] = data['Num loose-out-bailiwick glue'] > 0
-data['Has out-of-bailiwick glue'] = data['Num out-of-bailiwick glue'] > 0
-data['Has glue records'] = data['Num glue records'] > 0
-data['Has Mixed glue'] = data['Num glue records'] > data['Num out-of-bailiwick glue']
 
 def plot_upper_limit(axis):
     lims = [
-        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
-        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+        np.min([axis.get_xlim(), axis.get_ylim()]),  # min of both axes
+        np.max([axis.get_xlim(), axis.get_ylim()]),  # max of both axes
     ]
 
     # now plot both limits against eachother
     ylims = [0.0, 1.0]
-    ax.plot(lims, ylims, 'k-', alpha=0.75, zorder=0)
-    ax.set_aspect('equal')
-    ax.set_xlim(lims)
-    ax.set_ylim(lims)
+    axis.plot(lims, ylims, 'k-', alpha=0.75, zorder=0)
+    axis.set_aspect('equal')
+    axis.set_xlim(lims)
+    axis.set_ylim(lims)
+
 
 def normalize_cdf(series):
     return (series - series.min()) / series.count()
 
-plt.figure()
-fig, ax = plt.subplots()
-normalize_cdf(data['Has out-of-bailiwick glue'].astype(int).cumsum()).plot(label='has out-of-bailiwick glue')
-plot_upper_limit(ax)
-plt.title('CDF of out-of-bailiwick glue')
-plt.axis('tight')
-plt.legend()
-plt.savefig('cdf.png')
 
-plt.figure()
-fig, ax = plt.subplots()
-normalize_cdf(data['Has Mixed glue'].astype(int).cumsum()).plot(label='has mixed-bailiwick glue')
-plot_upper_limit(ax)
-plt.title('CDF of mixed-bailiwick glue')
-plt.axis('tight')
-plt.legend()
-plt.savefig('cdf.mixed.png')
+def read_multi_csv(files, **kwargs):
+    if not files:
+        files = ['-']
+
+    return pd.concat([
+            pd.read_csv(sys.stdin if f == '-' else f, **kwargs)
+            for f in files
+        ],
+        ignore_index=True
+    )
+
+
+def main():
+    data = read_multi_csv(sys.argv[1:])
+
+    data['Has loose-out-bailiwick glue'] = data['Num loose-out-bailiwick glue'] > 0
+    data['Has out-of-bailiwick glue'] = data['Num out-of-bailiwick glue'] > 0
+    data['Has glue records'] = data['Num glue records'] > 0
+    data['Has Mixed glue'] = data['Num glue records'] > data['Num out-of-bailiwick glue']
+
+    plt.figure()
+    fig, ax = plt.subplots()
+    normalize_cdf(data['Has out-of-bailiwick glue'].astype(int).cumsum()).plot(label='has out-of-bailiwick glue')
+    plot_upper_limit(ax)
+    plt.title('CDF of out-of-bailiwick glue')
+    plt.axis('tight')
+    plt.legend()
+    plt.savefig('cdf.png')
+
+    plt.figure()
+    fig, ax = plt.subplots()
+    normalize_cdf(data['Has Mixed glue'].astype(int).cumsum()).plot(label='has mixed-bailiwick glue')
+    plot_upper_limit(ax)
+    plt.title('CDF of mixed-bailiwick glue')
+    plt.axis('tight')
+    plt.legend()
+    plt.savefig('cdf.mixed.png')
+
+
+if __name__ == '__main__':
+    main()
