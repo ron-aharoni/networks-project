b4c0225745020bb216a561dd3cec0dd4b9d12333
Steven Karas <steven.karas@gmail.com>
2019-03-15 12:09:38 +0200

    script: split zone/dig scripts

---
+++ script.dig.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+
+# this script requires python 3.7.2 at a minimum
+#
+# Usage: cat dig.results | python script.dig.py
+# Usage: python script.dig.py dig.results 
+
+import fileinput
+from dataclasses import dataclass
+import typing
+
+
+@dataclass(frozen=True)
+class RR:
+    rrtype: typing.ClassVar[str] = NotImplemented
+    name: str
+    ttl: int
+    rrclass: str
+
+    @classmethod
+    def from_zone_line(cls, line):
+        try:
+            tokens = line.split()
+            # name ttl class rrtype rdata
+            line_rrtype = tokens[3]
+            for rrtype in cls.__subclasses__():
+                if rrtype.rrtype == line_rrtype:
+                    return rrtype.parse_zone_line(line)
+
+            # We ignore record types we don't care about
+            return None
+        except Exception:
+            print('Error parsing %s' % line)
+            return None
+
+
+@dataclass(frozen=True)
+class A(RR):
+    rrtype = 'A'
+    ip: str
+
+    @classmethod
+    def parse_zone_line(cls, line):
+        name, ttl, rrclass, rrtype, ip = line.split(maxsplit=4)
+        return cls(name=name, ttl=int(ttl), rrclass=rrclass, ip=ip)
+
+
+@dataclass(frozen=True)
+class AAAA(RR):
+    rrtype = 'AAAA'
+    ip: str
+
+    @classmethod
+    def parse_zone_line(cls, line):
+        name, ttl, rrclass, rrtype, ip = line.split(maxsplit=4)
+        return cls(name=name, ttl=int(ttl), rrclass=rrclass, ip=ip)
+
+
+@dataclass(frozen=True)
+class NS(RR):
+    rrtype = 'NS'
+    nameserver: str
+
+    @classmethod
+    def parse_zone_line(cls, line):
+        name, ttl, rrclass, rrtype, nameserver = line.split(maxsplit=4)
+        return cls(name=name, ttl=int(ttl), rrclass=rrclass, nameserver=nameserver)
+
+    def is_in_bailiwick(self):
+        return self.nameserver.endswith(self.name)
+
+    def shared_prefix(self):
+        result = []
+        for lhs, rhs in zip(reversed(self.name.split('.')), reversed(self.nameserver.split('.'))):
+            if lhs != rhs:
+                break
+            result.append(lhs)
+
+        return '.'.join(reversed(result))
+
+
+@dataclass(frozen=True)
+class CNAME(RR):
+    rrtype = 'CNAME'
+    canonical_name: str
+
+    @classmethod
+    def parse_zone_line(cls, line):
+        name, ttl, rrclass, rrtype, canonical_name = line.split(maxsplit=4)
+        return cls(name=name, ttl=int(ttl), rrclass=rrclass, canonical_name=canonical_name)
+
+
+@dataclass(frozen=True)
+class SOA(RR):
+    rrtype = 'SOA'
+    mname: str
+    rname: str
+    serial: int
+    refresh: int
+    retry: int
+    expire: int
+    neg_ttl: int
+
+    @classmethod
+    def parse_zone_line(cls, line):
+        name, ttl, rrclass, rrtype, mname, rname, serial, refresh, retry, expire, neg_ttl = line.split(maxsplit=10)
+        return cls(name=name, ttl=int(ttl), rrclass=rrclass, mname=mname, rname=rname, serial=int(serial), refresh=int(refresh), retry=int(retry), expire=int(expire), neg_ttl=int(neg_ttl))
+
+
+def main2():
+    num_ns_records = 0
+    num_glue_records = 0
+    num_out_of_bailiwick_glue = 0
+    num_loose_out_bailiwick_glue = 0
+    num_domains = 0
+    improper_glue = 0
+    ns_records = {}
+    glue_records = []
+    is_nx = False
+    num_nxdomain = 0
+    num_emptys = 0
+    has_soa = False
+    has_only_soa = True
+    num_soa_only = 0
+    import csv
+    output_file = 'raw_results.csv'
+    f = open(output_file, 'w')
+    writer = csv.writer(f)
+    writer.writerow(['Domain', 'Num NS records', 'Num glue records', 'Num out-of-bailiwick glue', 'Num loose-out-bailiwick glue'])
+    current_domain = None
+    for line in fileinput.input():
+        if line.startswith('; <<>> DiG 9.12.3 <<>> '):
+            current_domain = line.split()[5]
+
+        if line.startswith(';; ->>HEADER<<- opcode: QUERY, status: NXDOMAIN'):
+            is_nx = True
+
+        if line.startswith(';; MSG SIZE'):
+            if is_nx:
+                num_nxdomain += 1
+                if ns_records or glue_records:
+                    print('Anomolous NXDOMAIN with results: %s' % ns_records.values()[0].name)
+            if not ns_records and not glue_records:
+                num_emptys += 1
+
+            if has_soa:
+                if has_only_soa:
+                    num_soa_only += 1
+                else:
+                    print('Got SOA record in nonempty zone: %s' % current_domain)
+
+            num_domains += 1
+            domain_ns_records = len(ns_records)
+            num_ns_records += domain_ns_records
+
+            domain_glue_records = len(glue_records)
+            num_glue_records += domain_glue_records
+            domain_out_of_bailiwick_glue = 0
+            domain_loose_out_bailiwick_glue = 0
+            for glue in glue_records:
+                ns_record = ns_records.get(glue.name.lower())
+                if ns_record is None:
+                    improper_glue += 1
+                elif ns_record.is_in_bailiwick():
+                    pass
+                else:
+                    domain_out_of_bailiwick_glue += 1
+                    if ns_record.shared_prefix() == '':
+                        domain_loose_out_bailiwick_glue += 1
+
+            num_out_of_bailiwick_glue += domain_out_of_bailiwick_glue
+            num_loose_out_bailiwick_glue += domain_loose_out_bailiwick_glue
+
+            writer.writerow([current_domain, domain_ns_records, domain_glue_records, domain_out_of_bailiwick_glue, domain_loose_out_bailiwick_glue])
+
+            current_domain = None
+            has_soa = False
+            has_only_soa = True
+            is_nx = False
+            ns_records.clear()
+            glue_records = []
+
+        if line.startswith(';') or not line.strip():
+            continue
+
+        record = RR.from_zone_line(line.strip())
+        if record is None:
+            continue
+
+        if record.rrtype == NS.rrtype:
+            ns_records[record.nameserver.lower()] = record
+            has_only_soa = False
+
+        if record.rrtype == A.rrtype:
+            glue_records.append(record)
+            has_only_soa = False
+
+        if record.rrtype == SOA.rrtype:
+            has_soa = True
+
+    f.close()
+    print('Number of domains: %s' % num_domains)
+    print('NXDOMAINs: %s' % num_nxdomain)
+    print('Number of empty domains: %s' % num_emptys)
+    print('Number of NS records: %s' % num_ns_records)
+    print('Number of glue records: %s' % num_glue_records)
+    print('Number of improper glue records: %s' % improper_glue)
+    print('Out of bailiwick glue: %s' % num_out_of_bailiwick_glue)
+    print('Loosely out of bailiwick glue: %s' % num_loose_out_bailiwick_glue)
+    print('Number of SOA-only records: %s' % num_soa_only)
+
+
+if __name__ == '__main__':
+    main2()
